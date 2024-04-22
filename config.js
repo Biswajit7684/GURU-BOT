e0ab65b981ccc797a24bd4c8dab6aae19cda3ece
Biswajit7684 <129636033+Biswajit7684@users.noreply.github.com>
2024-04-22 08:03:13 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,16 +7,16 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['919556082678', '_*BISWAJIT*_', true],
+  ['918114900108', '_*BAPUN*_', true], 
   [''] 
 ] //Number of owners
 
 //global.pairingNumber = "" //put your bot number here
 
-global.mods = ['917605902011','32460248586'] 
-global.prems = ['917605902011', '32460248586', '919398758484']
-global.allowed = ['917605902011', '32460248586', '19152999993']
+global.mods = ['919556082678','918114900108'] 
+global.prems = ['919556082678', '918114900108', '919777722622']
+global.allowed = ['919556082678', '918114900108', '']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
@@ -50,16 +50,16 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = 'ᴛʜᴇ ɢᴜʀᴜ-ʙᴏᴛ'
+global.botname = 'T͛H͛U͛N͛D͛E͛R͛ M͛D͛'
 global.premium = 'true'
-global.packname = 'GURU┃ᴮᴼᵀ' 
-global.author = '@Asliguru' 
+global.packname = 'T͛H͛U͛N͛D͛E͛R͛┃ᴮᴼᵀ' 
+global.author = '@R͛A͛J͛A͛' 
 global.menuvid = 'https://i.imgur.com/AjDzGgG.mp4'
-global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/asli_guru69\n' 
-global.dygp = 'https://chat.whatsapp.com/BFfD1C0mTDDDfVdKPkxRAA'
-global.fgsc = 'https://github.com/Guru322/GURU-BOT' 
-global.fgyt = 'https://youtube.com/@Asliguru'
-global.fgpyp = 'https://youtube.com/@Asliguru'
+global.igfg = '▢ Follow on Instagram\n\n' 
+global.dygp = 'https://chat.whatsapp.com/LFoV5pjGPjk0F328b6Srms'
+global.fgsc = 'https://github.com/' 
+global.fgyt = 'https://youtube.com/@Smartstudyltd'
+global.fgpyp = 'https://youtube.com/@Smartstudyltd'
 global.fglog = 'https://raw.githubusercontent.com/Guru322/api/Guru/guru.jpg' 
 global.thumb = fs.readFileSync('./Assets/Gurulogo.jpg')
 
